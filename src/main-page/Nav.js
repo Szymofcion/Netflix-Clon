d593e0abd124bb542c13f902c1b94d3dbc6b2091
Szymon Dziewa <82220216+Szymofcion@users.noreply.github.com>
2023-02-28 13:37:55 +0100

    fixed

---
+++ src/main-page/Nav.js
@@ -0,0 +1,13 @@
+const Nav = () => {
+  return (
+    <div className="nav__container">
+      <span>Netlix</span> <a href="">Strona główna</a>
+      <a href="">seriale i programy</a>
+      <a href=""></a>
+      <a href=""></a>
+      <a href=""></a>
+      <a href=""></a>
+    </div>
+  );
+};
+export default Nav;
