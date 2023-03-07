e2331d75b808e46e28520b15b242e515c5d14588
Szymon Dziewa <82220216+Szymofcion@users.noreply.github.com>
2023-03-07 18:40:37 +0100

    added hero banner

--- src/main-page/HeroImg.js
+++ src/main-page/HeroImg.js
@@ -1,36 +1,50 @@
-import {useState, useEffect} from 'react'
-import { apiKey } from '../component/api/apiKey'
-import './HeroImg.scss'
+import { useState, useEffect } from "react";
+import { apiKey } from "../component/api/apiKey";
+import "./HeroImg.scss";
 
-const HeroImg = ()=>{
-    const [hero, setHero] =useState([])
+const HeroImg = () => {
+  const [hero, setHero] = useState([]);
 
-    useEffect(() => {
-        const headers = {
-          Autorization: apiKey,
-        };
-        const generateMovies = async () => {
-          const respons = await fetch(
-            "https://imdb-api.com/en/API/Images/k_nc812076/tt1375666/Short",
-            { headers }
-          );
-          const responseJSON = await respons.json();
-          const images = responseJSON.items;
-          console.log(images);
-          
-        //   Math.floor(Math.random() * images
-          setHero([...images]);
-        };
-        generateMovies();
-      }, []);
-    return(
-        <header className='container__img'>
-          <h1>title</h1>
-{/* {title} */}
-{/* {div>2buttons} */}
-{/* {descriptions} */}
-        </header>
-    )
-}
+  useEffect(() => {
+    const headers = {
+      Autorization: apiKey,
+    };
+    const generateMovies = async () => {
+      const respons = await fetch(
+        "https://imdb-api.com/en/API/Images/k_nc812076/tt1375666/Short",
+        { headers }
+      );
+      const responseJSON = await respons.json();
 
-export default HeroImg
+      setHero(
+        responseJSON.items[
+          Math.floor(Math.random() * responseJSON.items.length - 1)
+        ]
+      );
+    };
+    generateMovies();
+  }, []);
+  return (
+    <header className="container__img">
+      <div className="container__img-content">
+        <h1>{hero.title}</h1>
+        <div className="container__img-buttons">
+          <button>Play</button>
+          <button>My list</button>
+        </div>
+        <div className="container__img-descriptions"></div>
+        <span>
+          Czasy, gdy technologia pozwala na wchodzenie w świat snów. Złodziej
+          Cobb ma za zadanie wszczepić myśl do śpiącego umysłu.
+          Czasy, gdy technologia pozwala na wchodzenie w świat snów. Złodziej
+          Cobb ma za zadanie wszczepić myśl do śpiącego umysłu.
+          Czasy, gdy technologia pozwala na wchodzenie w świat snów. Złodziej
+          Cobb ma za zadanie wszczepić myśl do śpiącego umysłu.
+        </span>
+        {/* {descriptions} */}
+      </div>
+    </header>
+  );
+};
+
+export default HeroImg;
