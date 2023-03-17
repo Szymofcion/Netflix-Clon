127230ec5d41743f34c909aa310cd254b71d5af7
Szymon Dziewa <82220216+Szymofcion@users.noreply.github.com>
2023-03-17 17:31:20 +0100

    added movies

--- src/Microservice-netflix/app.js
+++ src/Microservice-netflix/app.js
@@ -19,24 +19,52 @@ app.get("/movies", function (req, res, next) {
       },
       {
         id: 2,
-        title: "Ty",
-        src: "http://localhost:3000/movieCover/Ty.jpg",
+        title: "EnolaHolmes2",
+        src: "http://localhost:3000/movieCover/EnolaHolmes2.jpg",
         descriptions:
-          "Błyskotliwy menadżer księgarni Joe (Penn Badgley) poznaje młodą aspirującą pisarkę Beck (Elizabeth Lail). Posługując się mediami społecznościowymi, mężczyzna zdobywa wiedzę na temat dziewczyny, by móc się do niej zbliżyć. Z czasem jego niewinne zauroczenie przeradza się w obsesję.",
+          "Enola przyjmuje pierwszą oficjalną sprawę jako detektyw, jednak aby rozwiązać tajemnicę zaginięcia dziewczyny, będzie potrzebować pomocy przyjaciół i brata Sherlocka.",
       },
       {
         id: 3,
-        title: "Ty",
-        src: "http://localhost:3000/movieCover/Ty.jpg",
+        title: "Fauda",
+        src: "http://localhost:3000/movieCover/Faud.jpg",
         descriptions:
-          "Błyskotliwy menadżer księgarni Joe (Penn Badgley) poznaje młodą aspirującą pisarkę Beck (Elizabeth Lail). Posługując się mediami społecznościowymi, mężczyzna zdobywa wiedzę na temat dziewczyny, by móc się do niej zbliżyć. Z czasem jego niewinne zauroczenie przeradza się w obsesję.",
+          "Akcja serialu toczy się we współczesnym Izraelu i w Palestynie na tle konfliktu izraelsko-palestyńskiego. W trakcie przesłuchania palestyńskiego członka Hamasu kapitan izraelskich służb specjalnych odkrywa, że Abu Ahmed, zwany też Panterą, terrorysta uznany za zabitego, żyje i szykuje zamac.",
       },
       {
         id: 4,
-        title: "Ty",
-        src: "http://localhost:3000/movieCover/Ty.jpg",
+        title: "GoodGirls`",
+        src: "http://localhost:3000/movieCover/GoodGirlL_.jpg",
         descriptions:
-          "Błyskotliwy menadżer księgarni Joe (Penn Badgley) poznaje młodą aspirującą pisarkę Beck (Elizabeth Lail). Posługując się mediami społecznościowymi, mężczyzna zdobywa wiedzę na temat dziewczyny, by móc się do niej zbliżyć. Z czasem jego niewinne zauroczenie przeradza się w obsesję.",
+          "Mieszkające na przedmieściach Detroit Beth (Christina Hendricks), Annie (Mae Whitman) i Ruby (Retta) ledwo wiążą koniec z końcem. Chcąc poprawić swoją sytuację finansową, organizują napad na sklep spożywczy, w którym Annie jest kasjerką.",
+      },
+      {
+        id: 5,
+        title: "JakSprzedawacDragiWSieci`",
+        src: "http://localhost:3000/movieCover/JakSprzedawacDragiWSieci.jpg",
+        descriptions:
+          "Aby odzyskać dziewczynę, nastoletni nerd zaczyna sprzedawać online ecstasy ze swojej sypialni i zostaje jednym z największych dilerów w Europie.",
+      },
+      {
+        id: 6,
+        title: "Luther`",
+        src: "http://localhost:3000/movieCover/Luther.jpg",
+        descriptions:
+          "rawurowo skonstruowany psychologiczny serial kryminalny opowiadający o piekielnie inteligentnym i stroniącym od ludzi Johnie Lutherze, który jest detektywem w londyńskiej jednostce policyjnej. ",
+      },
+      {
+        id: 7,
+        title: "Narcos`",
+        src: "http://localhost:3000/movieCover/Narcos.jpg",
+        descriptions:
+          "Pochodzący z Kolumbii Pablo Escobar (Wagner Moura) jest baronem narkotykowym. Oddany rodzinie mężczyzna szybko zbija fortunę na handlu kokainą.",
+      },
+      {
+        id: 8,
+        title: "Norsemen`",
+        src: "http://localhost:3000/movieCover/Norsemen.jpg",
+        descriptions:
+          "Norsemen to norweski serial, który opowiada o perypetiach mieszkańców wikińskiej osady Norheim. Powstał na fali popularności tematyką kultury i historii wikingów.",
       },
     ],
     rated: [
@@ -49,52 +77,108 @@ app.get("/movies", function (req, res, next) {
       },
       {
         id: 2,
-        title: "Zwerbowany",
-        src: "http://localhost:3000/movieCover/Zwerbowany.jpg",
+        title: "Luther`",
+        src: "http://localhost:3000/movieCover/Luther.jpg",
         descriptions:
-          "Młody prawnik zostaje wciągnięty w niebezpieczną międzynarodową aferę szpiegowską, gdy dawna pracownica CIA grozi, że ujawni sekrety agencji.",
+          "rawurowo skonstruowany psychologiczny serial kryminalny opowiadający o piekielnie inteligentnym i stroniącym od ludzi Johnie Lutherze, który jest detektywem w londyńskiej jednostce policyjnej. ",
       },
       {
         id: 3,
-        title: "Zwerbowany",
-        src: "http://localhost:3000/movieCover/Zwerbowany.jpg",
+        title: "JakSprzedawacDragiWSieci`",
+        src: "http://localhost:3000/movieCover/JakSprzedawacDragiWSieci.jpg",
         descriptions:
-          "Młody prawnik zostaje wciągnięty w niebezpieczną międzynarodową aferę szpiegowską, gdy dawna pracownica CIA grozi, że ujawni sekrety agencji.",
+          "Aby odzyskać dziewczynę, nastoletni nerd zaczyna sprzedawać online ecstasy ze swojej sypialni i zostaje jednym z największych dilerów w Europie.",
       },
       {
         id: 4,
-        title: "Zwerbowany",
-        src: "http://localhost:3000/movieCover/Zwerbowany.jpg",
+        title: "Obserwator",
+        src: "http://localhost:3000/movieCover/Obserwator.jpg",
         descriptions:
-          "Młody prawnik zostaje wciągnięty w niebezpieczną międzynarodową aferę szpiegowską, gdy dawna pracownica CIA grozi, że ujawni sekrety agencji.",
+          "Dean i Nora Brannockowie (w serialu Bobby Cannavale i Naomi Watts) przeprowadzają się do idyllicznej dzielnicy New Jersey, gdzie szukają spokoju i bezpiecznego miejsca dla swoich dzieci.",
+      },
+      {
+        id: 5,
+        title: "Peakty Blinders",
+        src: "http://localhost:3000/movieCover/PeakyBlinders.jpg",
+        descriptions:
+          "Tommy Shelby (Cillian Murphy) jest weteranem wojennym i przywódcą niesławnego gangu Peaky Blinders, zwanego tak z powodu żyletek wszytych do czapek noszonych przez jego członków.",
+      },
+      {
+        id: 6,
+        title: "SandMan",
+        src: "http://localhost:3000/movieCover/SandMan.jpg",
+        descriptions:
+          "Dean i Nora Brannockowie (w serialu Bobby Cannavale i Naomi Watts) przeprowadzają się do idyllicznej dzielnicy New Jersey, gdzie szukają spokoju i bezpiecznego miejsca dla swoich dzieci.",
+      },
+      {
+        id: 7,
+        title: "StrangerThings",
+        src: "http://localhost:3000/movieCover/StrangerThings.jpg",
+        descriptions:
+          "W nocy 6 listopada 1983, w Hawkins w stanie Indiana, w tajemniczych okolicznościach znika dwunastoletni Will Byers. Jego matka, Joyce, chce za wszelką cenę odnaleźć syna, część mieszkańców uważa jednak, że oszalała. ",
+      },
+      {
+        id: 8,
+        title: "TheRain",
+        src: "http://localhost:3000/movieCover/TheRain.jpg",
+        descriptions:
+          "Sześć lat po tym, jak roznoszony przez deszcz wirus spustoszył Skandynawię, brat i siostra dołączają do grupy młodych ocalałych, poszukując bezpieczeństwa — i odpowiedzi.",
       },
     ],
     comedy: [
       {
         id: 1,
-        title: "Zwerbowany",
-        src: "http://localhost:3000/movieCover/Zwerbowany.jpg",
+        title: "GoodGirls`",
+        src: "http://localhost:3000/movieCover/GoodGirlL_.jpg",
         descriptions:
-          "Młody prawnik zostaje wciągnięty w niebezpieczną międzynarodową aferę szpiegowską, gdy dawna pracownica CIA grozi, że ujawni sekrety agencji.",
+          "Mieszkające na przedmieściach Detroit Beth (Christina Hendricks), Annie (Mae Whitman) i Ruby (Retta) ledwo wiążą koniec z końcem. Chcąc poprawić swoją sytuację finansową, organizują napad na sklep spożywczy, w którym Annie jest kasjerką.",
       },
       {
         id: 2,
-        title: "Zwerbowany",
-        src: "http://localhost:3000/movieCover/Zwerbowany.jpg",
+        title: "SamceAlfa",
+        src: "http://localhost:3000/movieCover/SamceAlfa.jpg",
         descriptions:
-          "Młody prawnik zostaje wciągnięty w niebezpieczną międzynarodową aferę szpiegowską, gdy dawna pracownica CIA grozi, że ujawni sekrety agencji.",
+          "Piotrek za namową narzeczonej, zapisuje się na kurs rzucania palenia. Przez pomyłkę trafia na enigmatyczne szkolenie samorozwoju, gdzie pod wpływem charyzmatycznego coacha – Lidera, zaczyna poddawać w wątpliwość swój związek, który do tej pory uważał za idealny.",
       },
       {
         id: 3,
-        title: "Zwerbowany",
-        src: "http://localhost:3000/movieCover/Zwerbowany.jpg",
+        title: "NieMaJakWRodzinie",
+        src: "http://localhost:3000/movieCover/NieMaJakWRodzinie.jpg",
         descriptions:
           "Młody prawnik zostaje wciągnięty w niebezpieczną międzynarodową aferę szpiegowską, gdy dawna pracownica CIA grozi, że ujawni sekrety agencji.",
       },
       {
         id: 4,
-        title: "Zwerbowany",
-        src: "http://localhost:3000/movieCover/Zwerbowany.jpg",
+        title: "DobreMiejsce",
+        src: "http://localhost:3000/movieCover/DobreMiejsce.jpg",
+        descriptions:
+          "Młody prawnik zostaje wciągnięty w niebezpieczną międzynarodową aferę szpiegowską, gdy dawna pracownica CIA grozi, że ujawni sekrety agencji.",
+      },
+      {
+        id: 5,
+        title: "SexEducation",
+        src: "http://localhost:3000/movieCover/SexEducation.jpg",
+        descriptions:
+          "Młody prawnik zostaje wciągnięty w niebezpieczną międzynarodową aferę szpiegowską, gdy dawna pracownica CIA grozi, że ujawni sekrety agencji.",
+      },
+      {
+        id: 6,
+        title: "Atypowy",
+        src: "http://localhost:3000/movieCover/Atypowy.jpg",
+        descriptions:
+          "Młody prawnik zostaje wciągnięty w niebezpieczną międzynarodową aferę szpiegowską, gdy dawna pracownica CIA grozi, że ujawni sekrety agencji.",
+      },
+      {
+        id: 7,
+        title: "GinnyAndGorgia",
+        src: "http://localhost:3000/movieCover/GinnyAndGorgia.jpg",
+        descriptions:
+          "Młody prawnik zostaje wciągnięty w niebezpieczną międzynarodową aferę szpiegowską, gdy dawna pracownica CIA grozi, że ujawni sekrety agencji.",
+      },
+      {
+        id: 8,
+        title: "ParadisePD",
+        src: "http://localhost:3000/movieCover/ParadisePD.jpg",
         descriptions:
           "Młody prawnik zostaje wciągnięty w niebezpieczną międzynarodową aferę szpiegowską, gdy dawna pracownica CIA grozi, że ujawni sekrety agencji.",
       },
@@ -109,22 +193,50 @@ app.get("/movies", function (req, res, next) {
       },
       {
         id: 2,
-        title: "1899",
-        src: "http://localhost:3000/movieCover/1899.jpg",
+        title: "MamyTuDucha",
+        src: "http://localhost:3000/movieCover/MamyTuDucha.jpg",
         descriptions:
           "1899 rok. Parowiec z emigrantami bierze kurs na zachód. Pasażerowie pochodzą z różnych części Europy, ale łączą ich wspólne marzenia i nadzieja na lepszą przyszłość w nowym świecie.",
       },
       {
         id: 3,
-        title: "1899",
-        src: "http://localhost:3000/movieCover/1899.jpg",
+        title: "Opiekunka",
+        src: "http://localhost:3000/movieCover/Opiekunka.jpg",
         descriptions:
           "1899 rok. Parowiec z emigrantami bierze kurs na zachód. Pasażerowie pochodzą z różnych części Europy, ale łączą ich wspólne marzenia i nadzieja na lepszą przyszłość w nowym świecie.",
       },
       {
         id: 4,
-        title: "1899",
-        src: "http://localhost:3000/movieCover/1899.jpg",
+        title: "Rytual",
+        src: "http://localhost:3000/movieCover/Rytual.jpg",
+        descriptions:
+          "1899 rok. Parowiec z emigrantami bierze kurs na zachód. Pasażerowie pochodzą z różnych części Europy, ale łączą ich wspólne marzenia i nadzieja na lepszą przyszłość w nowym świecie.",
+      },
+      {
+        id: 5,
+        title: "Cisza",
+        src: "http://localhost:3000/movieCover/Cisza.jpg",
+        descriptions:
+          "1899 rok. Parowiec z emigrantami bierze kurs na zachód. Pasażerowie pochodzą z różnych części Europy, ale łączą ich wspólne marzenia i nadzieja na lepszą przyszłość w nowym świecie.",
+      },
+      {
+        id: 6,
+        title: "CoWidacISlychac",
+        src: "http://localhost:3000/movieCover/CoWidacISlychac.jpg",
+        descriptions:
+          "1899 rok. Parowiec z emigrantami bierze kurs na zachód. Pasażerowie pochodzą z różnych części Europy, ale łączą ich wspólne marzenia i nadzieja na lepszą przyszłość w nowym świecie.",
+      },
+      {
+        id: 7,
+        title: "Ladunek",
+        src: "http://localhost:3000/movieCover/Ladunek.jpg",
+        descriptions:
+          "1899 rok. Parowiec z emigrantami bierze kurs na zachód. Pasażerowie pochodzą z różnych części Europy, ale łączą ich wspólne marzenia i nadzieja na lepszą przyszłość w nowym świecie.",
+      },
+      {
+        id: 8,
+        title: "Clinical",
+        src: "http://localhost:3000/movieCover/Clinical.jpg",
         descriptions:
           "1899 rok. Parowiec z emigrantami bierze kurs na zachód. Pasażerowie pochodzą z różnych części Europy, ale łączą ich wspólne marzenia i nadzieja na lepszą przyszłość w nowym świecie.",
       },
@@ -139,24 +251,52 @@ app.get("/movies", function (req, res, next) {
       },
       {
         id: 2,
-        title: "Peakty Blinders",
-        src: "http://localhost:3000/movieCover/PeakyBlinders.jpg",
+        title: "Wiedzmin",
+        src: "http://localhost:3000/movieCover/Wiedzmin.jpg",
         descriptions:
-          "Tommy Shelby (Cillian Murphy) jest weteranem wojennym i przywódcą niesławnego gangu Peaky Blinders, zwanego tak z powodu żyletek wszytych do czapek noszonych przez jego członków.",
+          "Wiedźmin to epicka opowieść na podstawie kultowej sagi fantasy Andrzeja Sapkowskiego. Geralt z Rivii, samotny zabójca potworów, usiłuje odnaleźć się w świecie, w którym ludzie bywają gorsi niż bestie, na które poluje.",
       },
       {
         id: 3,
-        title: "Peakty Blinders",
-        src: "http://localhost:3000/movieCover/PeakyBlinders.jpg",
+        title: "GinnyAndGorgia",
+        src: "http://localhost:3000/movieCover/GinnyAndGorgia.jpg",
         descriptions:
-          "Tommy Shelby (Cillian Murphy) jest weteranem wojennym i przywódcą niesławnego gangu Peaky Blinders, zwanego tak z powodu żyletek wszytych do czapek noszonych przez jego członków.",
+          "Młody prawnik zostaje wciągnięty w niebezpieczną międzynarodową aferę szpiegowską, gdy dawna pracownica CIA grozi, że ujawni sekrety agencji.",
       },
       {
         id: 4,
-        title: "Peakty Blinders",
-        src: "http://localhost:3000/movieCover/PeakyBlinders.jpg",
+        title: "SamceAlfa",
+        src: "http://localhost:3000/movieCover/SamceAlfa.jpg",
         descriptions:
-          "Tommy Shelby (Cillian Murphy) jest weteranem wojennym i przywódcą niesławnego gangu Peaky Blinders, zwanego tak z powodu żyletek wszytych do czapek noszonych przez jego członków.",
+          "Piotrek za namową narzeczonej, zapisuje się na kurs rzucania palenia. Przez pomyłkę trafia na enigmatyczne szkolenie samorozwoju, gdzie pod wpływem charyzmatycznego coacha – Lidera, zaczyna poddawać w wątpliwość swój związek, który do tej pory uważał za idealny.",
+      },
+      {
+        id: 5,
+        title: "SandMan",
+        src: "http://localhost:3000/movieCover/SandMan.jpg",
+        descriptions:
+          "Dean i Nora Brannockowie (w serialu Bobby Cannavale i Naomi Watts) przeprowadzają się do idyllicznej dzielnicy New Jersey, gdzie szukają spokoju i bezpiecznego miejsca dla swoich dzieci.",
+      },
+      {
+        id: 6,
+        title: "StrangerThings",
+        src: "http://localhost:3000/movieCover/StrangerThings.jpg",
+        descriptions:
+          "W nocy 6 listopada 1983, w Hawkins w stanie Indiana, w tajemniczych okolicznościach znika dwunastoletni Will Byers. Jego matka, Joyce, chce za wszelką cenę odnaleźć syna, część mieszkańców uważa jednak, że oszalała. ",
+      },
+      {
+        id: 7,
+        title: "EnolaHolmes2",
+        src: "http://localhost:3000/movieCover/EnolaHolmes2.jpg",
+        descriptions:
+          "Enola przyjmuje pierwszą oficjalną sprawę jako detektyw, jednak aby rozwiązać tajemnicę zaginięcia dziewczyny, będzie potrzebować pomocy przyjaciół i brata Sherlocka.",
+      },
+      {
+        id: 8,
+        title: "Zwerbowany",
+        src: "http://localhost:3000/movieCover/Zwerbowany.jpg",
+        descriptions:
+          "Młody prawnik zostaje wciągnięty w niebezpieczną międzynarodową aferę szpiegowską, gdy dawna pracownica CIA grozi, że ujawni sekrety agencji.",
       },
     ],
   });
