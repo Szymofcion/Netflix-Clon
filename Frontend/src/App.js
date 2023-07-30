e22962c56ec53a7b57f97908f1586cd2a5ecdc90
unknown <szymon.dziewajs@gmail.com>
2023-07-30 14:52:36 +0200

    updated React-Redux

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,38 +1,17 @@
-import { Route, Switch, BrowserRouter } from "react-router-dom";
-import Welcome from "./component/welcome-page/Welcome";
-import Registration from "./registration-page/Registration";
-import Login from "./login-page/Login";
-import SelectProfil from "./select-profil-page/SelectProfil";
-import Main from "./main-page/Main";
-import { useState } from "react";
+
+import { useState } from 'react'
+import Welcome from './component/welcome-page/Welcome'
 
 function App() {
-  const [user, setUser] = useState(null);
-  const onLogin = (user) => {
-    setUser(user);
-  };
-  // const onLogout = () => {
-  //   setUser();
-  // };
+	const [user, setUser] = useState()
+	const onLogin = user => {
+		setUser(user)
+	}
+	// const onLogout = () => {
+	//   setUser();
+	// };
 
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/login">
-          {user ? <SelectProfil user={user} /> : <Login onLogin={onLogin} />}
-        </Route>
-        <Route path="/main">
-          <Main />
-        </Route>
-        <Route path="/registraion">
-          <Registration />
-        </Route>
-        <Route path="/">
-          <Welcome />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  );
+	return <Welcome/>
 }
 
-export default App;
+export default App
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,8 +1,42 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-import "./index.css";
-import App from "./App";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+import './index.css'
+
+import Registration from './registration-page/Registration'
+import Login from './login-page/Login'
+import SelectProfil from './select-profil-page/SelectProfil'
+import Main from './main-page/Main'
+import App from './App'
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App/>,
+	},
+	{
+		path: '/login',
+		element: <Login/>,
+	},
+	{
+		path: '/registration',
+		element: <Registration/>,
+	},
+	{
+		path: '/selectProfil',
+		element: <SelectProfil/>,
+	},
+	{
+		path: '/main',
+		element: <Main/>,
+	},
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+)
